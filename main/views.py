4afd9a5052467913b61469ec963d699d5038dfeb
drmartinlutherxing <drmartinlutherchang@gmail.com>
2017-01-14 14:42:21 -0800

    clean valid form data; added generic ListView, ContactsView class to display the model data; import Contacts and generic.ListView; add thanks page view;

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.edit import FormView
 
+
 from .forms import ContactForm
+from .models import Contacts
 
 
 def index(request):
@@ -12,6 +15,21 @@ def about(request):
     return render(request, "main/about.html")
 
 
+def thanks(request):
+    return render(request, "main/thanks.html")
+
+class ContactsView(ListView):
+    template_name = "main/contacts.html"
+    context_object_name = "contacts"
+    model = Contacts
+
+    def get_queryset(self):
+        """
+        Return all Contacts that have been submitted
+        """
+        return Contacts.objects.all()
+
+
 class ContactView(FormView):
     template_name = "main/contact.html"
     form_class = ContactForm
@@ -19,4 +37,9 @@ class ContactView(FormView):
 
     def form_valid(self, form):
         # Write code for after form validation
+        form.subject = form.cleaned_data['subject']
+        form.message = form.cleaned_data['message']
+        form.sender = form.cleaned_data['sender']
+
+        form.send_email()
         return super(ContactView, self).form_valid(form)
