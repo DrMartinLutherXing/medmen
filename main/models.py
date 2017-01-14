47ef408d01ddbfafda7f19bb8ab160123eab6fc1
drmartinlutherxing <drmartinlutherchang@gmail.com>
2017-01-14 14:15:40 -0800

    added model for Contacts and migrated sqlite db;

--- main/models.py
+++ main/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Contacts(models.Model):
+    subject = models.CharField(max_length=75)
+    message = models.CharField(max_length=200)
+    sender = models.EmailField()
+
 # Create your models here.
