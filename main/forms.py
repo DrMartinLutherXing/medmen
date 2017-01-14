ad365f8f58a67e26a06aed8b6ca03e8c2e7dc42f
drmartinlutherxing <drmartinlutherchang@gmail.com>
2017-01-14 14:40:17 -0800

    Save a new Contacts row based on valid form submission;

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.mail import send_mail
 
+from .models import Contacts
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=75)
@@ -11,12 +12,15 @@ class ContactForm(forms.Form):
 				"""
 				basic send_mail usage
 				"""
+				Contacts(
+				     subject=self.subject,
+						 message=self.message,
+						 sender=self.sender).save()
 				send_mail(
-					subject,
-					message,
-					'admin@example.com',
-					[sender],
-					fail_silently=False,
+					self.subject,
+					self.message,
+					'kieran.farrar@gmail.com',
+					[self.sender],
 				)
 				pass
 
