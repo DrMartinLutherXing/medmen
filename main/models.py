544cbd671f18fa4e1c909244b1eef28acd2981df
drmartinlutherxing <drmartinlutherchang@gmail.com>
2017-01-14 17:47:23 -0800

    added Customers model with fields for first name, last name. phone and email;

--- main/models.py
+++ main/models.py
@@ -2,9 +2,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+# Create your models here.
+
 class Contacts(models.Model):
     subject = models.CharField(max_length=75)
     message = models.CharField(max_length=200)
     sender = models.EmailField()
 
-# Create your models here.
+class Customers(models.Model):
+    fname = models.CharField(max_length=25)
+    lname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
