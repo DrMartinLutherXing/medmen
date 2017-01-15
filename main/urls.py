af77ebedc1b9bd22514ac76123289f8c2d500bdc
drmartinlutherxing <drmartinlutherchang@gmail.com>
2017-01-14 17:49:21 -0800

    added routes for editing, viewing, and deleting customers;

--- main/urls.py
+++ main/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^thanks/$', views.thanks, name="thanks"),
     url(r'^contact/$', views.ContactView.as_view(), name="contact"),
     url(r'^contacts/$', views.ContactsView.as_view(), name="contacts"),
+    url(r'^customer/(?P<customer_id>[0-9]+)/$', views.viewCustomer, name='customer'),
+    url(r'^customer/edit/(?P<customer_id>[0-9]+)/$', views.editCustomer, name='edit_customer'),
+    url(r'^customer/delete/(?P<customer_id>[0-9]+)/$', views.deleteCustomer, name='delete_customer'),
+    url(r'^customers/$', views.CustomersView.as_view(), name="customers"),
 ]
