ed520adb04b73a9410feba4aa8a2e2a077993d05
drmartinlutherxing <drmartinlutherchang@gmail.com>
2017-01-15 16:20:57 -0800

    changed Contacts model to account for textarea in form;

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Contacts(models.Model):
     subject = models.CharField(max_length=75)
-    message = models.CharField(max_length=200)
+    message = models.TextField(max_length=200)
     sender = models.EmailField()
 
 class Customers(models.Model):
